cfb7680549dcda36f22e42af1b92f51ba3caa464
Elysiocb <aldo.elysio@gmail.com>
2025-08-28 13:32:35 -0300

    �First Commit Ever

---
+++ ignoreMe.py
@@ -0,0 +1,3 @@
+print("wow, this have super confidential information of my program and i don't want to sell it out!")
+
+super_Confidential_Variable = 69
---
+++ test.py
@@ -0,0 +1,1 @@
+print("yay im using github for the first time!")
